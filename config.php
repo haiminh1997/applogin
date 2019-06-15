64d7e3c7462343a1ec1e3b98be3a4887bdfde30f
haiminh1997 <haiminh97lqd@gmail.com>
2019-06-15 17:03:10 +0700

    btvn

---
+++ config.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: HaiMinh
+ * Date: 6/15/2019
+ * Time: 8:31 AM
+ */
+/*
+ * hằng số kết nối đến csdl
+ */
+define("DB_SEVER", "localhost");
+define("DB_SEVER_USERNAME", "root");
+define("DB_SEVER_PASSWORD", "");
+define("DB_SEVER_NAME", "demoapplogin");
+/**
+ * mysqli_connect kết nối csdl
+ */
+$connection = mysqli_connect(DB_SEVER,DB_SEVER_USERNAME,DB_SEVER_PASSWORD,DB_SEVER_NAME);
+/**
+ * Kiểm tra xem kết nối đến csdl có thành công hay không
+ * nếu kết nối không thành công thì ngắt kết nối đến chương trình bằng câu lệnh die()
+ */
+if($connection == false){
+    die("ERROR Không thể kết nối đến CSDL" . mysqli_connect_error());
+}
